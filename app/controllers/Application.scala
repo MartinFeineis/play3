14c3c902946fe9e30fe6d5ec97b2440cd2277f85
MartinFeineis <martin.feineis@yahoo.de>
2015-10-29 22:59:22 -0400

    before Mongo University

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,9 +2,16 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import reactivemongo.api.{MongoConnection, MongoDriver}
 import sun.reflect.generics.reflectiveObjects.NotImplementedException
 import play.api.libs.json.Json
-// import controllers.mongoaction
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+
+import play.api.libs.iteratee.Iteratee
+
+import reactivemongo.bson.BSONDocument
+import reactivemongo.api.collections.bson.BSONCollection
 
 
 object Application extends Controller {
@@ -18,9 +25,50 @@ object Application extends Controller {
   def showThePullova = Action(Ok(Json.obj("pulli" -> Json.arr(Json.obj("name" -> "Wolle", "surname" -> "Pullova", "level" -> "Jedi"), Json.obj("name" -> "Batman", "surname" -> "Wayne", "level" -> "Justice League")))))
 
   def mongo = Action{
+    // see http://reactivemongo.org
+      println("Hello Mongo")
+      var dbpullis = new MongoDriver()
+      var connection: MongoConnection = dbpullis.connection(List("52.11.250.38"))
+      var db = connection("pullofans")
+      var collection = db.collection[BSONCollection]("firstfans")
+
+    def listDocs(collection: BSONCollection) = {
+      val query = BSONDocument()
+      // select only the fields 'lastName' and '_id'
+      val filter = BSONDocument()
+
+      /* Let's run this query then enumerate the response and print a readable
+       * representation of each document in the response */
+      collection.
+        find(query, filter).
+        cursor[BSONDocument].
+        enumerate().apply(Iteratee.foreach { doc =>
+       // println(s"found document: ${BSONDocument pretty doc}")
+      })
+
+      // Or, the same with getting a list
+      val futureList: Future[List[BSONDocument]] =
+        collection.
+          find(query, filter).
+          cursor[BSONDocument].
+          collect[List]()
+
+      futureList.map { list =>
+        list.foreach { doc =>
+         // println(s"found document: ${BSONDocument pretty doc}")
+        }
+      }
+    }
+    //val mdbdefi = connect()
+    // val mdbefo = new BSONCollection()
+    var mdebefa = listDocs(collection)
     var tisch = new table(2.3, 4, "holz")
     var tischname=  tisch.whois()
+
+
     println(tisch.n0legs().toString)
+    println(collection.count())
+
     Ok(views.html.pullova(tischname.toString))
   }
 }
